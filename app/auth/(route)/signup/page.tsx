9224056c28b50802a0eaa15931e47e80492b2000
dwhddnjs <per1215@naver.com>
2023-09-05 22:08:27 +0900

    ✨ 회원가입 페이지 추가

--- app/auth/(route)/signup/page.tsx
+++ app/auth/(route)/signup/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { postRequest } from "@/common/axios";
+import { API_KEYS } from "@/common/apiKeys";
 
 function SignupPage() {
   const formSchema = z.object({
@@ -11,10 +26,102 @@ function SignupPage() {
       .string()
       .min(1, { message: "최소 1글자 이상 작성해주세요" })
       .email("이메일 주소가 유효하지 않습니다"),
-    password: z.string().min(8),
-    checkPassword: z.string().min(8),
+    password: z.string().min(7),
+    checkPassword: z.string().min(7),
   });
-  return <div>SignupPage</div>;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      checkPassword: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const requestBody = {
+      name: values.name,
+      email: values.email,
+      password: values.password,
+    };
+    try {
+      const response = await postRequest(API_KEYS.signup, requestBody);
+      localStorage.setItem("access-token", response?.tokens?.access_token);
+      localStorage.setItem("refresh-token", response?.tokens?.refresh_token);
+    } catch (error) {
+      console.log("request", error);
+    }
+    return;
+  };
+
+  return (
+    <div className="flex justify-center items-center border-black border-2 h-screen">
+      <div className="border-2 border-black w-[400px] h-[500px]">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            onChange={() => console.log(form.getValues())}
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="checkPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Check Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">회원가입</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default SignupPage;
