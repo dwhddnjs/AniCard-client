04e1676b97fc316db44dcf28dd2fdb3681b18719
dwhddnjs <per1215@naver.com>
2023-10-06 23:42:46 +0900

    💄 랜딩페이지 css 수정

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,19 @@ export default function HomePage() {
   const { push } = useRouter();
 
   return (
-    <div className="w-full h-screen flex flex-row justify-between items-center p-[200px]">
-      <div className="flex flex-col justify-between border-2 space-y-44">
+    <div className="w-full h-screen flex flex-row justify-between items-center p-[200px] bg-gradient-to-b from-green-200 to-green-600">
+      <div className="flex flex-col justify-between space-y-40">
         <div>
-          <h3 className="text-4xl font-extrabold">
+          <h3 className="text-4xl font-extrabold ">
             당신의 강아지카드를 모아보세요
           </h3>
-          <p className="text-lg">어쩌구 저쩌구 사사사사사사삿사사사사사사</p>
+          <p className="text-lg text-gray-700">
+            어쩌구 저쩌구 사사사사사사삿사사사사사사
+          </p>
         </div>
         <Button onClick={() => push("/auth/login")}>시작하기</Button>
       </div>
-      <div>
+      <div className=" overflow-hidden rounded-3xl ">
         <Image src={landingDog} width={550} height={300} alt="" />
       </div>
     </div>
